1739f1b192f372d0f47387e44de22e51ea86c8a3
Caceresenzo <caceresenzo1502@gmail.com>
2024-06-09 15:24:15 +0200

    pass 1st stage

--- app/main.c
+++ app/main.c
@@ -1,15 +1,13 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main() {
-  // You can use print statements as follows for debugging, they'll be visible when running tests
-  printf("Logs from your program will appear here!\n");
+int main()
+{
+  printf("$ ");
+  fflush(stdout);
 
-  // Uncomment this block to pass the first stage
-  // printf("$ ");
-  // fflush(stdout);
-
-  // Wait for user input
   char input[100];
   fgets(input, 100, stdin);
-  return 0;
+
+  return (EXIT_SUCCESS);
 }
