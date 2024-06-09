1a14718afb49def6fcd8285043275676310f5e96
Caceresenzo <caceresenzo1502@gmail.com>
2024-06-09 15:28:54 +0200

    pass 2nd stage

--- app/main.c
+++ app/main.c
@@ -1,13 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
 
 int main()
 {
-  printf("$ ");
-  fflush(stdout);
+	printf("$ ");
+	fflush(stdout);
 
-  char input[100];
-  fgets(input, 100, stdin);
+	char input[100];
+	fgets(input, 100, stdin);
 
-  return (EXIT_SUCCESS);
+	
+	for (char *ptr = input; *ptr; ++ptr)
+	{
+		if (isspace(*ptr))
+		{
+			*ptr = '\0';
+			break;
+		}
+	}
+	
+	char *program = input;
+	printf("%s: command not found\n", program);
+
+	return (EXIT_SUCCESS);
 }
